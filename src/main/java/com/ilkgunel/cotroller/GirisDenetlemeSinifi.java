0c4652222c74e379d491e1077adc432e8541904a
İlkay Günel <ilkgunel93@gmail.com>
2015-08-17 13:13:26 +0300

    Spring Security Eklendi
    Kullanıcı yetkilendirme mekanizması olan Spring Security projeye
    eklendi ve test edildi.

---
+++ src/main/java/com/ilkgunel/cotroller/GirisDenetlemeSinifi.java
@@ -0,0 +1,57 @@
+package com.ilkgunel.cotroller;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.application.NavigationHandler;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@ManagedBean
+@SessionScoped
+public class GirisDenetlemeSinifi {
+	
+	public void login()  
+	{
+
+        System.out.println("Login Metoduna Girildi");
+        try {
+
+            ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+            RequestDispatcher dispatcher = ((ServletRequest)context.getRequest()).getRequestDispatcher("/j_spring_security_check");
+
+            dispatcher.forward((ServletRequest)context.getRequest(), (ServletResponse)context.getResponse());
+            FacesContext.getCurrentInstance().responseComplete();
+           
+        } catch (ServletException | IOException ex) {
+            Logger.getLogger(GirisDenetlemeSinifi.class.getName()).log(Level.SEVERE, null, ex);
+
+        } 
+        /*finally {
+            return null;
+        }*/
+        System.out.println("Login Metodundan Çıkıldı");
+    }
+	
+	public void authorizedUserControl(){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(!authentication.getPrincipal().toString().equals("anonymousUser")){
+
+            NavigationHandler nh =  FacesContext.getCurrentInstance().getApplication().getNavigationHandler();
+            nh.handleNavigation(FacesContext.getCurrentInstance(), null, "/icerikEkle.xhtml?faces-redirect=true");
+
+        }
+    }
+}
---
+++ src/main/java/com/ilkgunel/cotroller/Test.java
@@ -0,0 +1,28 @@
+package com.ilkgunel.cotroller;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Test {
+	public static void main(String[] args)
+	{
+	String parola = "12345";
+	    
+	    try{
+	        MessageDigest messageDigestNesnesi = MessageDigest.getInstance("MD5");
+	        messageDigestNesnesi.update(parola.getBytes());
+	        byte messageDigestDizisi[] = messageDigestNesnesi.digest();
+	        StringBuffer sb16 = new StringBuffer();
+	        StringBuffer sb32 = new StringBuffer();
+	        for (int i = 0; i < messageDigestDizisi.length; i++) {
+	        sb16.append(Integer.toString((messageDigestDizisi[i] & 0xff) + 0x100, 16).substring(1));
+	        sb32.append(Integer.toString((messageDigestDizisi[i] & 0xff) + 0x100, 32));
+	 }
+	 System.out.println("Parolanın Şifrelenmiş Hali:(16) " + sb16.toString());
+	 System.out.println("Parolanın Şifrelenmiş Hali:(32) " + sb32.toString());
+	    }
+	    catch(NoSuchAlgorithmException ex){
+	        System.err.println(ex);
+	    }
+	}
+}
